c23b02b3ad2743e5c30feb2a9595a88c68b66295
Talal <talalothmanse@gmail.com>
2023-03-15 03:34:40 +0800

    -Minor fix for tailwindcss bug

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
 import { ref } from 'vue'
+
+const purchasePrice = ref(0)
+const downPayment = ref(0)
+const repaymentTime = ref(0)
+const interestRate = ref(0)
+const loanAmount = ref(0)
+const monthlyPayment = ref(0)
+
 </script>
 
 <template>
-
+  <div class="flex">
+    <label for="purchasePrice">Purchase Price</label> {{ purchasePrice }}
+    <input  type="range" name="purchasePrice" min="0" max="1000000" step="50000" v-model="purchasePrice">
+  </div>
 </template>
 
