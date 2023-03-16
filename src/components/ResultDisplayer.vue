0995d29f74579d6cde550a4afef54d0f235dc9dd
Talal <talalothmanse@gmail.com>
2023-03-17 04:27:03 +0800

    -New component

---
+++ src/components/ResultDisplayer.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['result', 'title'])
+</script>
+
+<template>
+    <div class="flex flex-col items-center p-5">
+        <p>{{title}}</p>
+        <p>{{ result.toLocaleString() }} $</p>
+    </div>
+</template>
+
+
+
